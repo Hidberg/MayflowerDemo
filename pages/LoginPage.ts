db8ce85e0d092ed9ac54a0f2ab53ee4bc48bef31
Hidberg <hidberg@yandex.ru>
2026-06-18 16:19:11 +0300

    waitFor spinner on login

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,12 +5,14 @@ export class LoginPage {
     readonly usernameInput: Locator;
     readonly passwordInput: Locator;
     readonly loginButton: Locator;
+    readonly spinLoader: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.usernameInput = page.locator('input[name="username"]');
         this.passwordInput = page.locator('input[name="password"]');
         this.loginButton = page.locator('button[type="submit"]');
+        this.spinLoader = page.locator('.loader');
     }
 
     async goto() {
@@ -20,6 +22,7 @@ export class LoginPage {
     async login(username: string, password: string) {
         await this.usernameInput.fill(username);
         await this.passwordInput.fill(password);
+        await this.spinLoader.waitFor({ state: 'hidden' });
         await this.loginButton.click();
     }
 }
